6d1837d4dc494e0f427b03cbc2917d5fb1265070
sherelene <sherelenedebelen@gmail.com>
2022-04-22 10:01:38 -0700

    code before checking and changing code in case of redundancy

--- newton_introp.py
+++ newton_introp.py
@@ -9,31 +9,52 @@ import os
 
 
 def coeff(xs, ys, n, z, ex):
-    ex3_n = 'k'
+    """
+    This function does newton's interpolation
+    The xs is for data values of x
+    The ys is for data values of y
+    The n is the number of data sets
+    The ex is used to store which exercise number of the homework we're running
+    """
+
+    # initialize ex3_n variable for keeping record which exercise we are running
+    ex4_10s = 'k'
+
+    # initialize and declare new data array to store the y's
     cs = np.zeros(n + 1, dtype=np.dtype(Decimal))
     for i in range(n + 1):
         cs[i] = ys[i]
-
+    # Try except in case there is a zero division error
     try:
-        tic = time.perf_counter()
+        tic = time.perf_counter()   # begin count of time interpolation is running
         for j in range(1, n):
             for i in range(n - 1, j - 1, -1):
-                cs[i] = (cs[i] - cs[i - 1]) / (xs[i] - xs[i - j])
-        toc = time.perf_counter()
+                cs[i] = (cs[i] - cs[i - 1]) / (xs[i] - xs[i - j])   # formula for doing interpolation
+        toc = time.perf_counter()   # end count of time interpolation is running
     except ZeroDivisionError:
         print("Program ran through a division by zero and could not be avoided")
     else:
-        if ex == 4:
+        if ex == 4:  # if program is running exercise 4
             inter_time = toc - tic
-        elif ex == 3:
+        elif ex == 4.1:   # if program is running exercise 4 but with list on 10^k
             inter_time = toc - tic
-            ex3_n = 3
+            ex4_10s = 4.1
         else:
             inter_time = 'k'
-        eval_newton(xs, cs, n, z, ex, inter_time, ex3_n)
+        eval_newton(xs, cs, n, z, ex, inter_time, ex4_10s)
 
 
-def eval_newton(xs, cs, num, z, ex, interp_time, ex3_n):
+def eval_newton(xs, cs, num, z, ex, interp_time, ex4_10s):
+    """
+        This function does newton's interpolation evaluated
+        The xs is for data values of x
+        The cs is for data values of computed y's
+        The num is number of data sets
+        The z is the number the point should be evaluated at
+        The ex is used to store which exercise number of the homework we're running
+        The interp_time is used to store the time interpolation took in seconds
+        The ex4_10s is used to store exercise number 4 pt 2 of using
+        """
     tic = time.perf_counter()
     result = cs[num]
     for i in range(num - 1, -1, -1):
@@ -41,24 +62,24 @@ def eval_newton(xs, cs, num, z, ex, interp_time, ex3_n):
     toc = time.perf_counter()
     if ex == 4:
         eval_time = toc - tic
-    elif ex == 3:
+    elif ex == 4.1:
         eval_time = toc - tic
-        ex3_n = 3
+        ex4_10s = 4.1
     else:
         inter_time = 'k'
-    print_to_file(result, interp_time, eval_time, ex, num, ex3_n)
+    print_to_file(result, interp_time, eval_time, ex, num, ex4_10s)
 
 
-def print_to_file(solution, interpo_time, evalu_time, ex, n, ex3_n):
+def print_to_file(solution, interpo_time, evalu_time, ex, n, ex4_10s):
     input_filename = args.filename
-    if ex3_n == 3:
+    if ex4_10s == 4.1:
         output_filename = str(n) + ".pnt"
     else:
         output_filename = os.path.splitext(input_filename)[0] + ".pnt"
 
     with open(output_filename, "w") as external_file:
         print(solution, file=external_file)
-        if ex == 4 or ex3_n == 3:
+        if ex == 4 or ex4_10s == 4.1:
             print('Duration of evaluation in seconds: {}'.format(interpo_time), file=external_file)
             print('Duration of evaluation in seconds: {}'.format(evalu_time), file=external_file)
         external_file.close()
@@ -88,7 +109,7 @@ def clean_file_data(num_of_variables, file_list):
 
 
 def create_data_points(n):
-    ex = 3
+    ex = 4.1
     x = []
     y = []
 
@@ -100,7 +121,7 @@ def create_data_points(n):
 
     coeff(x, y, n, z, ex)
 
-def exercise3():
+def exercise4_10s():
     exercise = [10, 100, 1000]
     for i in exercise:
         create_data_points(i)
@@ -115,8 +136,8 @@ def check_argument(args):
 
 def main(args):
     filename = check_argument(args)
-    if args.ex3:
-        exercise3()
+    if args.ex4:
+        exercise4_10s()
     elif filename == -1:
         with open(args.filename) as file:
             # declare array we're going to get from the file
@@ -153,7 +174,8 @@ if __name__ == '__main__':
                     '<int>')
     parser.add_argument('-n', '--n', type=int, help="creates random data points of size input int",
                         required=False)
-    parser.add_argument('-ex3', '--ex3', action="store_true", help="calls function do do exercise 3 of homework",
+    parser.add_argument('-ex4', '--ex4', action="store_true", help="calls function to do exercise 4 of homework"
+                                                                       "where it runs ints of 10, 100, 1000",
                         required=False)
     parser.add_argument("filename", help="stores filename")
 
