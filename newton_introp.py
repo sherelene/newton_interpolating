4f2fc5d4d2e516a66447b80cf14b2d0937f64ce3
sherelene <sherelenedebelen@gmail.com>
2022-04-22 02:04:27 -0700

    added a timer to check speed of code

--- newton_introp.py
+++ newton_introp.py
@@ -1,27 +1,55 @@
+import random
 import numpy as np
 from decimal import *
+import time
 
 
-def coeff(xs, ys, cs, num):
-    print(num)
-    for i in range(num):
+
+
+def coeff(xs, ys, n, z, ex):
+    cs = np.zeros(n+1, dtype=np.dtype(Decimal))
+    for i in range(n+1):
         cs[i] = ys[i]
-        print(f'cs {i} {cs[i]}')
-    print("number", num)
 
-    for j in range(1, num):
-        for i in range(num-1, j, -1):
-            print(f'i {i} - j {j} = {i - j}')
-            print(f'x{i}({cs[i]}) - x{i-1}({cs[i - 1]}) / y{i}({xs[i]}) - y{i-j}({xs[i-j]})')
-            cs[i] = (cs[i] - cs[i-1]) / (xs[i] - xs[i-j])
-            print("=", i, cs[i])
+    try:
+        tic = time.perf_counter()
+        for j in range(1, n):
+            for i in range(n - 1, j - 1, -1):
+                    cs[i] = (cs[i] - cs[i - 1]) / (xs[i] - xs[i - j])
+        toc = time.perf_counter()
+    except ZeroDivisionError:
+        print("Program ran through a division by zero and cannot be avoided")
+    else:
+        if ex == 4:
+            print('Duration of interpolation in seconds: {}'.format(toc-tic))
+        eval_newton(xs, cs, n, z, ex)
 
-    eval_newton(xs, cs, num)
+
+def eval_newton(xs, cs, num, z, ex):
+
+    tic = time.perf_counter()
+    result = cs[num]
+    for i in range(num-1, -1, -1):
+        result = result * (z - xs[i]) + cs[i]
+    toc = time.perf_counter()
+
+    if ex == 4:
+        print('Duration of evaluation in seconds: {}'.format(toc - tic))
+    print(result)
 
 
-def eval_newton(xs, cs, num):
+
+def clean_file_data(num_of_variables, file_list):
+    ex = 0
+    # initialize new array to store the solved coefficients
+    x = np.zeros(num_of_variables+1, dtype=np.dtype(Decimal))
+    y = np.zeros(num_of_variables+1, dtype=np.dtype(Decimal))
+
+    for j in range(num_of_variables):
+        x[j] = file_list[0][j]
+        y[j] = file_list[1][j]
+
     z = 0
-    print("num", num)
     user_input = input("Please enter a value to evaluate: ")
     try:
         if user_input == 'q':
@@ -31,29 +59,21 @@ def eval_newton(xs, cs, num):
         print("Error, should have been a number or 'q'")
         exit()
 
-    result = cs[num - 1]
-    for i in range(num-1, -1, -1):
-        result = result * (z - xs[i]) + cs[i]
+    coeff(x, y, num_of_variables, z, ex)
 
-    print(result)
+def create_data_points(n):
+    ex = 4
+    x = []
+    y = []
 
+    for it in range(n+1):
+        x.append(round(random.uniform(0.1, 1000), 2))  # outputs an 3x2 array filled with random floating point
+    # values.
+        y.append(round(random.uniform(0.1, 1000), 2))
+    z = random.randint(1, n-1)
+    print("n =", n)
 
-
-def create_arrays(num_of_variables, file_list):
-
-    # initialize new array to store the solved coefficients
-    x = np.zeros(num_of_variables, dtype=np.dtype(Decimal))
-    y = np.zeros(num_of_variables, dtype=np.dtype(Decimal))
-    c = np.zeros(num_of_variables, dtype=np.dtype(Decimal))
-
-    for j in range(num_of_variables):
-        x[j] = file_list[0][j]
-        y[j] = file_list[1][j]
-    print("x", x)
-    print("y", y)
-
-    coeff(x, y, c, num_of_variables)
-
+    coeff(x, y, n, z, ex)
 
 
 def main():
@@ -80,11 +100,17 @@ def main():
             num_list[1][i] = float(content[1][i])
 
         print(n)
-        create_arrays(n, num_list)
+        clean_file_data(n, num_list)
 
 
 
 if __name__ == '__main__':
 
-    main()
+    #main()
+    exercise = [10, 100, 1000]
+    for i in exercise:
+        create_data_points(i)
+
+
+
 
